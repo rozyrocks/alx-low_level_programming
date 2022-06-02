c0d86ac1bda02914764d98cfe3d6ecd0199018d8
Modupe <ehinsdupe@yahoo.com>
2022-06-02 16:43:38 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-#include <stdio.h> int main (void) {puts("Programming is like building a multilingual puzzle\n"; return (0);}
+#include <stdio.h>
+/**
+ * main - main block
+ * Retur: 0
+ */
+int main(void)
+
+{
+
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
