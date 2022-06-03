84b1b6f5094e7cc88cd9237198c95050f4f3c2c9
Modupe <ehinsdupe@yahoo.com>
2022-06-03 09:51:48 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if (n > 0)
-printf("%d is positive\n",
+	printf("%d is positive\n",
 else if (n == 0)
-printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 else
-printf("%d is negative\n",
+	printf("%d is negative\n",
 
 return (0);
 }
