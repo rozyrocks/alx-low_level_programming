5833200f66d6466e20889f3fdf2b850bde9bad30
Modupe <ehinsdupe@yahoo.com>
2022-06-03 12:25:43 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: random number in n +ve/-ve
+ * Return: 0 on Success
+ */
+
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
