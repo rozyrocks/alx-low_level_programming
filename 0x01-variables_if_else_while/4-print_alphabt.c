8e38d1745c3a5ab95e743384912ecf45752cb63e
rozyrocks <105121305+rozyrocks@users.noreply.github.com>
2022-06-03 20:05:34 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: random nymber in n +ve/-ve
- * Return: Always 0n (Succes)
+ *main - entry point
+ *Description: random number in n +ve/-ve
+ *Return: Always 0 (success)
  */
+
 int main(void)
+
 {
+
 int firstLetter, n;
+
 for (firstLetter = 97, n = 1; n < 27; firstLetter++, n++)
+
 {
-if (firstLetter == 100 || firstLetter == 113)
+
+if (firstLetter == 101 || firstLetter == 113)
+
 continue;
+
 putchar(firstLetter);
-}
-putchar(10);
-return (0);
+
 }
 
+putchar(10);
+
+return (0);
+
+}
