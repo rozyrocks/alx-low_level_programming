7d45024a4a93ba1b8c1510492e3288d8841b52a4
Modupe <ehinsdupe@yahoo.com>
2022-06-03 14:11:11 -0700

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,32 +1,20 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 /**
  *main - Entry point
- *Description: random number in n +ve/-ve
+ *Description:prints all single digit numbers of base 10
+ *starting from 0, followed by a new line.
  *Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-
-int n;
-
-for (n = 0; n < 10; n++)
-
+char c = '0';
+while (c <= '9')
 {
-
-int n;
-
-for (n = 0; n < 10; n++)
-
-{
-
-printf("%d", n);
-
+putchar(c);
+c++;
 }
-
-
+putchar('\n');
 return (0);
-
 }
