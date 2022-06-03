3b4aee476b1e20df1717a74639ed4f448160fa7b
Modupe <ehinsdupe@yahoo.com>
2022-06-02 17:10:34 -0700

    Printf.c git commit -m Printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-{#include<stdio.h> int main(void) {printf("with proper grammar, but the outcome is a piece of art,\n"); return (0);}}
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a place of art,
+");
+	return (0);
+}
+	
