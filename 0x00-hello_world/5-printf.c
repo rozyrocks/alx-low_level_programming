76215bfeb204bf5282d16f0cb682ed15c7bf28ac
Modupe <ehinsdupe@yahoo.com>
2022-06-02 18:07:46 -0700

    Printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: 0
- */
-int main(void)
-{
-	printf("\"with proper grammar, but the outcome is a place of art,
-");
-	return (0);
+int main(void) {
+	printf("with proper grammar, but the outcome is a place of art,");
 }
+	return (0);
+	printf("\n");
+}
+return 0;
 	
