8b063e74f590e1196e1355a7b98771be042c23fa
Modupe <ehinsdupe@yahoo.com>
2022-06-04 11:08:45 -0700

    Printf. c git commit -m Printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
- *main - print string
+ *main - prints "with proper grammar, but the outcome is
+ *               a piece of art, "followed by a new line.
  *
  * Return: Always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a place of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a place of art,\n");
+
+	return (0);
 }
 
