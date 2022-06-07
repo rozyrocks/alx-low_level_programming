a8501f7fd06ba948048217c8051d876b16d98b0c
Modupe <ehinsdupe@yahoo.com>
2022-06-07 14:19:54 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * ma - prints the alphabet in lowercase, followed by a new line.
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
