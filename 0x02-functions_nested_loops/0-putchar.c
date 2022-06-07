1d61358f1f9d6e0e4039229053fca52da3c653ef
Modupe <ehinsdupe@yahoo.com>
2022-06-07 12:39:57 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,12 @@
-#include <unistd.h>
+#include <unistd.>
 
 /**
- * main - prints "_putchar" followed by a new line.
+ *main- prints "_putchar" followed by a new line.
  *
  * Return: Always 0.
  */
-int main(void)
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write!, &c, 1));
 }
-
 
