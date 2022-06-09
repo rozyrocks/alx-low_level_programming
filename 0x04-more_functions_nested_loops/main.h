49122b2f047f1b48fd016b7fb7666e46efa7e458
Modupe <ehinsdupe@yahoo.com>
2022-06-09 10:52:12 -0700

    Main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
+ */
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+
+#endif /* MAIN_H */
