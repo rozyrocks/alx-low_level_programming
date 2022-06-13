e42efa83603817badd452b4325d0309fe30710d5
Modupe <ehinsdupe@yahoo.com>
2022-06-13 15:38:07 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * puts2 - skips 2
- * @str: string
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
  */
 void puts2(char *str)
 {
-	int i;
+	int count = 0;
 
-	i = 0;
-	while (str[i] != '\0')
+	while (count >= 0)
 	{
-		if (i % 2 == 0)
-			_putchar(str[i];
-					i++;
-					}
-					_putchar('\n');
-					}
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count];
+					count++;
+	}
+}
