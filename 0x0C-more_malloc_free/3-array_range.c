da93c08fd1b13909916240829cff1cd4a34ece85
Modupe <ehinsdupe@yahoo.com>
2022-06-30 16:31:08 -0700

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,29 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - allocates memory for an array.
- * @nmemb: number of elements.
- * @size: size of bytes.
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
  *
- * Return: pointer to the allocated memory.
- * if nmemb or size is 0, returns NULL.
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
  * if malloc fails, returns NULL.
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+int *array_range(int min, int max)
 {
-	char *p;
-	unsigned int i;
+	int *ar;
+	int i;
 
-	if (nmemb == 0 || size == 0)
+	if (min > max)
 		return (NULL);
 
-	p = malloc(nmemb * size);
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 
-	if (p == NULL)
+	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = 0;
+	for (i = 0; min <= max; i++, min++)
+		ar[i] = min;
 
-	return (p);
+	return (ar);
 }
